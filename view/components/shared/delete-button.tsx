a1bdf8e5fa79e9fc63ae12efba37ce7085493afe
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-08-29 11:12:03 -0400

    feat: improve styles for delete button

--- view/components/shared/delete-button.tsx
+++ view/components/shared/delete-button.tsx
@@ -1,14 +1,22 @@
-import { Button } from '../ui/button';
+import { cn } from '@/lib/shared.utils';
 import { ButtonHTMLAttributes } from 'react';
+import { Button } from '../ui/button';
 
 interface DeleteButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-export const DeleteButton = ({ children, className, ...buttonProps }: DeleteButtonProps) => (
-  <Button 
-    variant="outline" 
-    className={`w-full text-destructive border-destructive hover:bg-destructive hover:text-destructive-foreground ${className || ''}`}
+export const DeleteButton = ({
+  children,
+  className,
+  ...buttonProps
+}: DeleteButtonProps) => (
+  <Button
+    variant="outline"
+    className={cn(
+      'border-destructive text-destructive hover:text-destructive-foreground w-full',
+      className,
+    )}
     {...buttonProps}
   >
     {children}
