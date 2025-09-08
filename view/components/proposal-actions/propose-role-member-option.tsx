6c0fec047fde778c9f69e706d2d9ef92dfd60b45
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-09-08 12:44:31 -0400

    feat: add ProposeRoleMemberOption wip component

---
+++ view/components/proposal-actions/propose-role-member-option.tsx
@@ -0,0 +1,46 @@
+import { User } from '@/types/user.types';
+import { RoleMemberOption } from '../roles/role-member-option';
+
+// TODO: Ensure correct types are used - remove any
+interface Props {
+  member: User;
+  selectedMembers: any[];
+  setSelectedMembers(selectedMembers: User[]): void;
+  currentRoleMembers?: User[];
+}
+
+export const ProposeRoleMemberOption = ({
+  member,
+  selectedMembers,
+  setSelectedMembers,
+  currentRoleMembers,
+}: Props) => {
+  const isSelectedToAdd = selectedMembers.some(
+    ({ userId, changeType }) => userId === member.id && changeType === 'add',
+  );
+  const isSelectedToRemove = selectedMembers.some(
+    ({ userId, changeType }) => userId === member.id && changeType === 'remove',
+  );
+  const isAlreadyAdded = currentRoleMembers?.some(({ id }) => id === member.id);
+
+  const checked = (isAlreadyAdded && !isSelectedToRemove) || isSelectedToAdd;
+
+  const handleChange = () => {
+    if ((!isAlreadyAdded && checked) || (isAlreadyAdded && !checked)) {
+      setSelectedMembers(
+        selectedMembers.filter(({ userId }) => userId !== member.id),
+      );
+      return;
+    }
+    const changeType = isAlreadyAdded && checked ? 'remove' : 'add';
+    setSelectedMembers([...selectedMembers, { changeType, userId: member.id }]);
+  };
+
+  return (
+    <RoleMemberOption
+      handleChange={handleChange}
+      checked={checked}
+      user={member}
+    />
+  );
+};
