134d589ee7a6be685d031e4f9eff3227d6289018
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-09-10 15:37:18 -0400

    feat: resolve type error in create proposal form

--- view/components/proposals/create-proposal-form/create-proposal-form.tsx
+++ view/components/proposals/create-proposal-form/create-proposal-form.tsx
@@ -39,7 +39,7 @@ export const CreateProposalForm = ({
     defaultValues: {
       body: '',
       action: '',
-      permissions: {},
+      permissions: [],
       roleMembers: [],
       selectedRoleId: '',
     },
