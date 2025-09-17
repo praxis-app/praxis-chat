fb4f3ad815a46982817619d7b149499501212c4e
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-09-17 19:30:49 -0400

    refactor: move serverConfigSchema to common

--- src/server-configs/middleware/validate-server-config.middleware.ts
+++ src/server-configs/middleware/validate-server-config.middleware.ts
@@ -1,42 +1,6 @@
-import { DECISION_MAKING_MODEL } from '@common/proposals/proposal.constants';
-import { VotingTimeLimit } from '@common/votes/vote.constants';
+import { serverConfigSchema } from '@common/server-configs/server-config.types';
 import { NextFunction, Request, Response } from 'express';
 import * as zod from 'zod';
-
-// TODO: Determine whether this should be moved to common
-const serverConfigSchema = zod
-  .object({
-    decisionMakingModel: zod.enum(DECISION_MAKING_MODEL).optional(),
-    disagreementsLimit: zod.number().min(0).max(10).optional(),
-    abstainsLimit: zod.number().min(0).max(10).optional(),
-    ratificationThreshold: zod.number().min(1).max(100).optional(),
-    votingTimeLimit: zod.number().optional(),
-  })
-  .refine(
-    (data) =>
-      !(
-        data.decisionMakingModel === 'majority-vote' &&
-        data.ratificationThreshold !== undefined &&
-        data.ratificationThreshold <= 50
-      ),
-    {
-      message:
-        'Ratification threshold must be greater than 50 for majority vote',
-      path: ['ratificationThreshold'],
-    },
-  )
-  .refine(
-    (data) =>
-      !(
-        data.decisionMakingModel === 'consent' &&
-        data.votingTimeLimit === VotingTimeLimit.Unlimited
-      ),
-    {
-      message:
-        'Voting time limit must be set for consent decision making model',
-      path: ['votingTimeLimit'],
-    },
-  );
 
 export const validateServerConfig = (
   req: Request,
--- view/components/settings/proposal-settings-form.tsx
+++ view/components/settings/proposal-settings-form.tsx
@@ -2,6 +2,7 @@ import { api } from '@/client/api-client';
 import { Button } from '@/components/ui/button';
 import { ServerConfigReq, ServerConfigRes } from '@/types/server-config.types';
 import { DECISION_MAKING_MODEL } from '@common/proposals/proposal.constants';
+import { serverConfigSchema } from '@common/server-configs/server-config.types';
 import { VotingTimeLimit } from '@common/votes/vote.constants';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
@@ -33,46 +34,12 @@ interface Props {
   serverConfig: ServerConfigRes;
 }
 
-const proposalSettingsFormSchema = zod
-  .object({
-    decisionMakingModel: zod.enum(DECISION_MAKING_MODEL).optional(),
-    disagreementsLimit: zod.number().min(0).max(10).optional(),
-    abstainsLimit: zod.number().min(0).max(10).optional(),
-    ratificationThreshold: zod.number().min(1).max(100).optional(),
-    votingTimeLimit: zod.number().optional(),
-  })
-  .refine(
-    (data) =>
-      !(
-        data.decisionMakingModel === 'majority-vote' &&
-        data.ratificationThreshold !== undefined &&
-        data.ratificationThreshold <= 50
-      ),
-    {
-      message:
-        'Ratification threshold must be greater than 50 for majority vote',
-      path: ['ratificationThreshold'],
-    },
-  )
-  .refine(
-    (data) =>
-      !(
-        data.decisionMakingModel === 'consent' &&
-        data.votingTimeLimit === VotingTimeLimit.Unlimited
-      ),
-    {
-      message:
-        'Voting time limit must be set for consent decision making model',
-      path: ['votingTimeLimit'],
-    },
-  );
-
 export const ProposalSettingsForm = ({ serverConfig }: Props) => {
   const { t } = useTranslation();
   const queryClient = useQueryClient();
 
-  const form = useForm<zod.infer<typeof proposalSettingsFormSchema>>({
-    resolver: zodResolver(proposalSettingsFormSchema),
+  const form = useForm<zod.infer<typeof serverConfigSchema>>({
+    resolver: zodResolver(serverConfigSchema),
     defaultValues: serverConfig,
     mode: 'onChange',
   });
