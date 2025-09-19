e3475cb4a2af69bb3e551c10eff2628862f7938d
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-09-19 01:56:07 -0400

    feat: ensure test proposals can still be created

--- view/components/proposals/create-proposal-form/create-proposal-form.types.ts
+++ view/components/proposals/create-proposal-form/create-proposal-form.types.ts
@@ -17,7 +17,9 @@ export const createProposalFormSchema = zod.object({
   roleName: zod.string().optional(),
   roleColor: zod.string().optional(),
   action: zod.enum([...PROPOSAL_ACTION_TYPE, '']),
-  permissions: zod.record(zod.enum(PERMISSION_KEYS), zod.boolean()).optional(),
+  permissions: zod
+    .record(zod.enum(PERMISSION_KEYS), zod.boolean().optional())
+    .optional(),
   roleMembers: zod.array(zod.string()).optional(),
   selectedRoleId: zod.string().optional(),
 });
