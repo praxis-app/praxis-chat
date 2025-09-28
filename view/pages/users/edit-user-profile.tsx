16559216254b02ad86c66e7c70735fd26893c4f6
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-09-28 03:39:07 -0400

    feat: rm header for user profile form

--- view/pages/users/edit-user-profile.tsx
+++ view/pages/users/edit-user-profile.tsx
@@ -1,17 +1,10 @@
 import { TopNav } from '@/components/nav/top-nav';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+import { Card, CardContent } from '@/components/ui/card';
 import { UserProfileForm } from '@/components/users/user-profile-form';
 import { NavigationPaths } from '@/constants/shared.constants';
 import { useIsDesktop } from '@/hooks/use-is-desktop';
 import { useMeQuery } from '@/hooks/use-me-query';
 import { useAppStore } from '@/store/app.store';
-import { VisuallyHidden } from '@radix-ui/react-visually-hidden';
 import { useTranslation } from 'react-i18next';
 import { MdClose } from 'react-icons/md';
 import { useNavigate } from 'react-router-dom';
@@ -39,7 +32,7 @@ export const EditUserProfile = () => {
   return (
     <>
       <TopNav
-        header={t('users.headers.editProfile')}
+        header={t('users.actions.editProfile')}
         onBackClick={handleBackClick}
         backBtnIcon={<MdClose className="size-6" />}
         goBackOnEscape
@@ -47,15 +40,6 @@ export const EditUserProfile = () => {
 
       <div className="flex h-full flex-col items-center justify-center p-4 md:p-18">
         <Card className="w-full max-w-md">
-          <CardHeader>
-            <VisuallyHidden>
-              <CardTitle>{t('users.headers.editProfile')}</CardTitle>
-            </VisuallyHidden>
-            <CardDescription>
-              {t('users.headers.editProfileDescription')}
-            </CardDescription>
-          </CardHeader>
-
           <CardContent>
             <UserProfileForm currentUser={meData.user} />
           </CardContent>
