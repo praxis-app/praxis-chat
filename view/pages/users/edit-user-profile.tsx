4d2dabb823a8b234fecf0df02d33bb49ded95fac
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-09-28 02:25:08 -0400

    feat: add top nav for edit profile page

--- view/pages/users/edit-user-profile.tsx
+++ view/pages/users/edit-user-profile.tsx
@@ -1,3 +1,34 @@
+import { useNavigate } from 'react-router-dom';
+import { TopNav } from '../../components/nav/top-nav';
+import { NavigationPaths } from '../../constants/shared.constants';
+import { useIsDesktop } from '../../hooks/use-is-desktop';
+import { useTranslation } from 'react-i18next';
+import { useAppStore } from '../../store/app.store';
+import { MdClose } from 'react-icons/md';
+
 export const EditUserProfile = () => {
-  return <div>EditUserProfile</div>;
+  const { setIsNavSheetOpen } = useAppStore();
+
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+  const isDesktop = useIsDesktop();
+
+  const handleBackClick = () => {
+    if (isDesktop) {
+      navigate(NavigationPaths.Home);
+      return;
+    }
+    setIsNavSheetOpen(true);
+  };
+
+  return (
+    <>
+      <TopNav
+        header={t('users.actions.editProfile')}
+        onBackClick={handleBackClick}
+        backBtnIcon={<MdClose className="size-6" />}
+        goBackOnEscape
+      />
+    </>
+  );
 };
