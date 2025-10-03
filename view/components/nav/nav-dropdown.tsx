adaca36e18f2edf445a45186753042aa73c7208a
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-10-03 15:04:25 -0400

    feat: show user profile in dialog in mobile nav

--- view/components/nav/nav-dropdown.tsx
+++ view/components/nav/nav-dropdown.tsx
@@ -3,14 +3,21 @@ import { NavigationPaths } from '@/constants/shared.constants';
 import { useMeQuery } from '@/hooks/use-me-query';
 import { truncate } from '@/lib/text.utils';
 import { useAppStore } from '@/store/app.store';
+import { VisuallyHidden } from '@radix-ui/react-visually-hidden';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { ReactNode, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { MdExitToApp, MdPerson } from 'react-icons/md';
 import { useNavigate } from 'react-router-dom';
-import { toast } from 'sonner';
 import { LogOutDialogContent } from '../auth/log-out-dialog-content';
-import { Dialog, DialogTrigger } from '../ui/dialog';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '../ui/dialog';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -18,6 +25,7 @@ import {
   DropdownMenuTrigger,
 } from '../ui/dropdown-menu';
 import { UserAvatar } from '../users/user-avatar';
+import { UserProfile } from '../users/user-profile';
 
 interface Props {
   trigger: ReactNode;
@@ -55,51 +63,66 @@ export const NavDropdown = ({ trigger }: Props) => {
   const truncatedUsername = truncate(name, 22);
 
   return (
-    <Dialog open={showLogoutDialog} onOpenChange={setShowLogoutDialog}>
-      <DropdownMenu>
-        <DropdownMenuTrigger>{trigger}</DropdownMenuTrigger>
-        <DropdownMenuContent
-          sideOffset={12}
-          className="mr-2.5 flex flex-col gap-2 p-3"
-        >
-          <DropdownMenuItem
-            className="text-md"
-            onClick={() => toast(t('prompts.inDev'))}
-          >
-            <UserAvatar
-              name={name}
-              userId={me.id}
-              imageSrc={me.profilePicture?.url}
-              className="size-5"
-              fallbackClassName="text-[0.7rem]"
-            />
-            {truncatedUsername}
-          </DropdownMenuItem>
-
-          {!me.anonymous && (
-            <DropdownMenuItem
-              onClick={() => navigate(NavigationPaths.UsersEdit)}
-              className="text-md"
-            >
-              <MdPerson className="text-foreground size-5" />
-              {t('users.actions.editProfile')}
-            </DropdownMenuItem>
-          )}
-
+    <DropdownMenu>
+      <DropdownMenuTrigger>{trigger}</DropdownMenuTrigger>
+      <DropdownMenuContent
+        sideOffset={12}
+        className="mr-2.5 flex flex-col gap-2 p-3"
+      >
+        <Dialog>
           <DialogTrigger asChild>
-            <DropdownMenuItem className="text-md">
+            <DropdownMenuItem
+              className="text-md"
+              onSelect={(e) => e.preventDefault()}
+            >
+              <UserAvatar
+                name={name}
+                userId={me.id}
+                imageSrc={me.profilePicture?.url}
+                className="size-5"
+                fallbackClassName="text-[0.7rem]"
+              />
+              {truncatedUsername}
+            </DropdownMenuItem>
+          </DialogTrigger>
+          <DialogContent>
+            <VisuallyHidden>
+              <DialogHeader>
+                <DialogTitle>{truncatedUsername}</DialogTitle>
+                <DialogDescription>{me.bio}</DialogDescription>
+              </DialogHeader>
+            </VisuallyHidden>
+            <UserProfile user={me} />
+          </DialogContent>
+        </Dialog>
+
+        {!me.anonymous && (
+          <DropdownMenuItem
+            onClick={() => navigate(NavigationPaths.UsersEdit)}
+            className="text-md"
+          >
+            <MdPerson className="text-foreground size-5" />
+            {t('users.actions.editProfile')}
+          </DropdownMenuItem>
+        )}
+
+        <Dialog open={showLogoutDialog} onOpenChange={setShowLogoutDialog}>
+          <DialogTrigger asChild>
+            <DropdownMenuItem
+              className="text-md"
+              onSelect={(e) => e.preventDefault()}
+            >
               <MdExitToApp className="text-foreground size-5" />
               {t('auth.actions.logOut')}
             </DropdownMenuItem>
           </DialogTrigger>
-        </DropdownMenuContent>
-      </DropdownMenu>
-
-      <LogOutDialogContent
-        handleLogout={logOut}
-        isPending={isLogoutPending}
-        setShowLogoutDialog={setShowLogoutDialog}
-      />
-    </Dialog>
+          <LogOutDialogContent
+            handleLogout={logOut}
+            isPending={isLogoutPending}
+            setShowLogoutDialog={setShowLogoutDialog}
+          />
+        </Dialog>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
