42bd522d365a39e91b17329a30b9a2138ba6239f
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-10-07 17:44:58 -0400

    refactor: add TODOs for moving shared constants and leveraging zod for backend validation in sign-up form

--- view/components/auth/sign-up-form.tsx
+++ view/components/auth/sign-up-form.tsx
@@ -26,6 +26,8 @@ import {
 } from '../ui/form';
 import { Input } from '../ui/input';
 
+// TODO: Move shared constants to @common
+
 const EMAIL_MAX_LENGTH = 254;
 
 const NAME_REGEX = /^[A-Za-z0-9 ]+$/;
@@ -35,6 +37,7 @@ const NAME_MAX_LENGTH = 15;
 const PASSWORD_MIN_LENGTH = 8;
 const PASSWORD_MAX_LENGTH = 64;
 
+// TODO: Leverage zod for BE validation
 const signUpFormSchema = zod
   .object({
     name: zod
