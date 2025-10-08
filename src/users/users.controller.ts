2c841541d4c6c2a98c1937c2482ba215e8934912
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-10-07 23:14:27 -0400

    refactor: remove old TODO comments

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -28,31 +28,25 @@ export const updateUserProfile = async (req: Request, res: Response) => {
 };
 
 export const createUserProfilePicture = async (req: Request, res: Response) => {
-  // TODO: Move validation to middleware
   if (!req.file) {
     res.status(422).send('No image uploaded');
     return;
   }
-
   const image = await usersService.createUserProfilePicture(
     req.file.filename,
     res.locals.user.id,
   );
-
   res.status(201).json({ image });
 };
 
 export const createUserCoverPhoto = async (req: Request, res: Response) => {
-  // TODO: Move validation to middleware
   if (!req.file) {
     res.status(422).send('No image uploaded');
     return;
   }
-
   const image = await usersService.createUserCoverPhoto(
     req.file.filename,
     res.locals.user.id,
   );
-
   res.status(201).json({ image });
 };
