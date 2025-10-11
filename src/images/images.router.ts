cb646979d952c7bc4de6e974fd740f891781b636
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-10-10 20:49:44 -0400

    refactor: rm images router

--- src/images/images.router.ts
+++
@@ -1,8 +0,0 @@
-// TODO: Guard routes with permission checks
-
-import express from 'express';
-import * as imagesController from './images.controller';
-
-export const imagesRouter = express.Router();
-
-imagesRouter.get('/:id', imagesController.getImageFile);
