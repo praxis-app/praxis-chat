62000f685132d1fb5d4c4ce00ed7bd95a754ff10
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-10-29 17:42:22 -0400

    feat: improve model descriptions in ollama.types.ts, add tiny llama option

--- src/chat-analysis/ollama/ollama.types.ts
+++ src/chat-analysis/ollama/ollama.types.ts
@@ -2,13 +2,15 @@ import { ChatRequest } from 'ollama';
 
 /** Ollama models that are available to use for chat analysis */
 export type Model =
-  // Powerful open source model released by OpenAI - https://ollama.com/library/gpt-oss:20b
+  // Powerful, open source model released by OpenAI - https://ollama.com/library/gpt-oss:20b
   | 'gpt-oss:20b'
-  // Lightweight text only model released by Meta - https://ollama.com/library/llama3.2:3b
+  // Lightweight, text only model released by Meta - https://ollama.com/library/llama3.2:3b
   | 'llama3.2:3b'
-  // Very small text only model released by Meta - https://ollama.com/library/llama3.2:1b
+  // Very small, text only model released by Meta - https://ollama.com/library/llama3.2:1b
   | 'llama3.2:1b'
-  // Very small text only model released by Google - https://ollama.com/library/gemma3:1b
+  // Very small, text only, open source model - https://ollama.com/library/tinyllama:1.1b
+  | 'tinyllama:1.1b'
+  // Very small, text only model released by Google - https://ollama.com/library/gemma3:1b
   | 'gemma3:1b';
 
 export interface PromptTemplate
