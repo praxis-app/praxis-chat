3adbca81fe7375a3d08045ed0c920480c1a880df
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-11-06 13:08:41 -0500

    test: ensure tests pass for chat summary

--- src/chat-analysis/ollama/ollama.types.ts
+++ src/chat-analysis/ollama/ollama.types.ts
@@ -2,14 +2,12 @@ import { ChatRequest } from 'ollama';
 
 /** Ollama models that are available to use for chat analysis */
 export type Model =
-  // Small, text only model released by Meta, 1.3GB RAM - https://ollama.com/library/llama3.2:1b
+  // Small, text only model released by Meta - https://ollama.com/library/llama3.2:1b
   | 'llama3.2:1b'
-  // Small, text only model released by Google, 815MB RAM - https://ollama.com/library/gemma3:1b
-  | 'gemma3:1b'
-  // Small, text only model released by Google, 1.6GB RAM - https://ollama.com/library/gemma2:2b
-  | 'gemma2:2b'
-  // Small, text only model released by Alibaba, 986MB RAM - https://ollama.com/library/qwen2.5:1.5b
-  | 'qwen2.5:1.5b';
+  // Small, text only model released by Alibaba - https://ollama.com/library/qwen:1.8b
+  | 'qwen:1.8b'
+  // Small, text only model released by Google - https://ollama.com/library/gemma2:2b
+  | 'gemma2:2b';
 
 export interface PromptTemplate
   extends Pick<ChatRequest, 'format' | 'options'> {
