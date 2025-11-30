08f7475feefb1c8022cf69c968e487f852e8200b
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-11-30 03:30:26 -0500

    feat: add wip instance settings page

---
+++ view/pages/settings/instance-settings.tsx
@@ -0,0 +1,49 @@
+// TODO: Add remaining layout and functionality - below is a WIP
+
+import { TopNav } from '@/components/nav/top-nav';
+import { SettingsNavItem } from '@/components/settings/settings-nav-item';
+import { Container } from '@/components/ui/container';
+import { NavigationPaths } from '@/constants/shared.constants';
+import { useIsDesktop } from '@/hooks/use-is-desktop';
+import { useAppStore } from '@/store/app.store';
+import { useTranslation } from 'react-i18next';
+import { MdAdminPanelSettings, MdClose } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
+import { useServerData } from '../../hooks/use-server-data';
+
+export const InstanceSettings = () => {
+  const { setIsNavSheetOpen } = useAppStore();
+
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+  const isDesktop = useIsDesktop();
+
+  const { serverPath } = useServerData();
+
+  const handleBackClick = () => {
+    if (isDesktop) {
+      navigate(serverPath);
+      return;
+    }
+    setIsNavSheetOpen(true);
+  };
+
+  return (
+    <>
+      <TopNav
+        header={t('navigation.headers.instanceSettings')}
+        onBackClick={handleBackClick}
+        backBtnIcon={<MdClose className="size-6" />}
+        goBackOnEscape
+      />
+
+      <Container className="flex flex-col gap-4.5">
+        <SettingsNavItem
+          Icon={MdAdminPanelSettings}
+          label={t('navigation.labels.roles')}
+          to={NavigationPaths.ServerRoles}
+        />
+      </Container>
+    </>
+  );
+};
