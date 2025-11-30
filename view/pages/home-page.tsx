0eb4ad7af71aa83ef1daecb022c61c87df08ec86
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-11-29 20:13:42 -0500

    feat: redirect from home to server route

--- view/pages/home-page.tsx
+++ view/pages/home-page.tsx
@@ -1,12 +1,22 @@
 import { ChannelSkeleton } from '@/components/channels/channel-skeleton';
 import { ChannelView } from '@/components/channels/channel-view';
+import { Navigate, useLocation } from 'react-router-dom';
+import { NavigationPaths } from '../constants/shared.constants';
 import { useGeneralChannel } from '../hooks/use-general-channel';
+import { useServerData } from '../hooks/use-server-data';
 
 export const HomePage = () => {
   const { data: channelData, error: channelError } = useGeneralChannel();
 
+  const { serverPath } = useServerData();
+  const { pathname } = useLocation();
+
   if (channelError) {
     throw new Error(channelError.message);
+  }
+
+  if (pathname === NavigationPaths.Home) {
+    return <Navigate to={serverPath} />;
   }
 
   if (!channelData) {
