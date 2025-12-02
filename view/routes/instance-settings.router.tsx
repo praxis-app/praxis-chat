04fece4c716e60950114dc6b3d580fe3e322a89d
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-12-01 19:34:02 -0500

    refactor: add view/pages/settings/instance-settings folder - rename

--- view/routes/instance-settings.router.tsx
+++ view/routes/instance-settings.router.tsx
@@ -1,8 +1,8 @@
 // TODO: Add remaining layout and functionality - below is a WIP
 
-import { EditInstanceRolePage } from '@/pages/settings/instance-roles/edit-instance-role-page';
-import { InstanceRoles } from '@/pages/settings/instance-roles/instance-roles';
-import { InstanceSettings } from '@/pages/settings/instance-roles/instance-settings';
+import { EditInstanceRolePage } from '@/pages/settings/instance-settings/edit-instance-role-page';
+import { InstanceRoles } from '@/pages/settings/instance-settings/instance-roles';
+import { InstanceSettings } from '@/pages/settings/instance-settings/instance-settings';
 import { RouteObject } from 'react-router-dom';
 
 export const instanceSettingsRouter: RouteObject = {
