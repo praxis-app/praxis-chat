4b07d3ba5ae7cb91f861a069ae823a779f4c27f9
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-12-30 00:36:12 -0500

    feat: enhance avatar styling in ServerListItem with dynamic colors

--- view/components/servers/server-list-item.tsx
+++ view/components/servers/server-list-item.tsx
@@ -4,6 +4,8 @@ import { NavigationPaths } from '@/constants/shared.constants';
 import { useIsDesktop } from '@/hooks/use-is-desktop';
 import { cn } from '@/lib/shared.utils';
 import { ServerRes } from '@/types/server.types';
+import chroma from 'chroma-js';
+import ColorHash from 'color-hash';
 import { useTranslation } from 'react-i18next';
 import { MdArrowForwardIos } from 'react-icons/md';
 import { Link } from 'react-router-dom';
@@ -22,13 +24,27 @@ export const ServerListItem = ({ server }: Props) => {
     return (value?.trim()?.[0] || '?').toUpperCase();
   };
 
+  const getStringAvatarProps = () => {
+    const colorHash = new ColorHash();
+    const baseColor = colorHash.hex(server.id || server.name);
+    const color = chroma(baseColor).brighten(1.5).hex();
+    const backgroundColor = chroma(baseColor).darken(1.35).hex();
+
+    return {
+      style: { color, backgroundColor },
+    };
+  };
+
   return (
     <Link to={editPath}>
       <div className="hover:bg-ring/10 cursor-pointer rounded-lg p-2 transition-colors">
         <div className="flex items-center justify-between gap-3">
           <div className="flex items-center gap-3">
             <Avatar className="size-10">
-              <AvatarFallback className="bg-accent font-semibold uppercase">
+              <AvatarFallback
+                className="text-lg font-light uppercase"
+                {...getStringAvatarProps()}
+              >
                 {getInitial(server.name)}
               </AvatarFallback>
             </Avatar>
@@ -36,11 +52,11 @@ export const ServerListItem = ({ server }: Props) => {
             <div className="flex flex-col">
               <div
                 className={cn(
-                  'flex items-center gap-2 truncate leading-tight font-medium',
+                  'flex items-center gap-2 truncate leading-tight',
                   isAboveMd ? 'max-w-[520px]' : 'max-w-[240px]',
                 )}
               >
-                <span className="truncate">{server.name}</span>
+                <span className="truncate font-medium">{server.name}</span>
                 <span className="text-muted-foreground truncate text-xs font-normal">
                   /s/{server.slug}
                 </span>
