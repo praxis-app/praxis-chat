db9c0aca64d8c8b8ab51b05d9f816f2236851446
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-12-30 02:21:03 -0500

    feat: truncate server descriptin in ServerListItem

--- view/components/servers/server-list-item.tsx
+++ view/components/servers/server-list-item.tsx
@@ -16,7 +16,7 @@ interface Props {
 
 export const ServerListItem = ({ server }: Props) => {
   const { t } = useTranslation();
-  const isAboveMd = useIsDesktop();
+  const isDesktop = useIsDesktop();
 
   const editPath = `${NavigationPaths.ManageServers}/${server.id}/edit`;
 
@@ -37,52 +37,49 @@ export const ServerListItem = ({ server }: Props) => {
 
   return (
     <Link to={editPath}>
-      <div className="hover:bg-ring/10 cursor-pointer rounded-lg p-2 transition-colors">
-        <div className="flex items-center justify-between gap-3">
-          <div className="flex items-center gap-3">
-            <Avatar className="size-10">
-              <AvatarFallback
-                className="text-lg font-light uppercase"
-                {...getStringAvatarProps()}
-              >
-                {getInitial(server.name)}
-              </AvatarFallback>
-            </Avatar>
+      <div className="hover:bg-ring/10 flex cursor-pointer items-center justify-between gap-3 rounded-lg p-2 transition-colors">
+        <div className="flex min-w-0 items-center gap-3">
+          <Avatar className="size-10">
+            <AvatarFallback
+              className="text-lg font-light uppercase"
+              {...getStringAvatarProps()}
+            >
+              {getInitial(server.name)}
+            </AvatarFallback>
+          </Avatar>
 
-            <div className="flex flex-col">
-              <div
-                className={cn(
-                  'flex items-center gap-2 truncate leading-tight',
-                  isAboveMd ? 'max-w-[520px]' : 'max-w-[240px]',
-                )}
-              >
-                <span className="truncate font-medium">{server.name}</span>
-                <span className="text-muted-foreground truncate text-xs font-normal">
-                  /s/{server.slug}
-                </span>
-                {server.isDefaultServer && (
-                  <Badge variant="secondary" className="uppercase">
-                    {t('servers.labels.default')}
-                  </Badge>
-                )}
+          <div className="w-full">
+            <div
+              className={cn(
+                'flex items-center gap-2 truncate leading-tight',
+                server.name.length > 20 &&
+                  !isDesktop &&
+                  'flex-col items-stretch gap-0',
+              )}
+            >
+              <div className="truncate font-medium">{server.name}</div>
+              <div className="text-muted-foreground truncate text-xs font-normal">
+                /s/{server.slug}
               </div>
-
-              {server.description && (
-                <p
-                  className={cn(
-                    'text-muted-foreground truncate text-xs leading-tight',
-                    isAboveMd ? 'max-w-[520px]' : 'max-w-[240px]',
-                  )}
-                  title={server.description}
-                >
-                  {server.description}
-                </p>
+              {server.isDefaultServer && (
+                <Badge variant="secondary" className="uppercase">
+                  {t('servers.labels.default')}
+                </Badge>
               )}
             </div>
-          </div>
 
-          <MdArrowForwardIos className="text-muted-foreground size-5 shrink-0" />
+            {server.description && (
+              <p
+                className="text-muted-foreground truncate pr-14 text-xs leading-tight"
+                title={server.description}
+              >
+                {server.description}
+              </p>
+            )}
+          </div>
         </div>
+
+        <MdArrowForwardIos className="text-muted-foreground size-5 shrink-0" />
       </div>
     </Link>
   );
